7900f6a026b86fcc38a1bd1d6709b9c46bc5ea29
marvin <marvin010528@foxmail.com>
2023-12-05 10:51:30 +0800

    memo

--- src/views/memo/index.tsx
+++ src/views/memo/index.tsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, {useEffect, useMemo, useState} from "react";
 
 export const Memo = () => {
+    console.log('rerender')
+
+    const [userInfo, setUserInfo] = useState({
+        username: 'marvin',
+        password: '123456'
+    })
+
+    const [count, setCount] = useState(0)
+
+
+    const id = useMemo(() => {
+        console.log('aa')
+        return userInfo.username + userInfo.password
+    }, [userInfo]);
+
+    useEffect(() => {
+        console.log('userInfo', userInfo)
+    }, [userInfo]);
+
+
     return (
         <>
-            memo
+            <strong>{id}</strong>
+            <strong>{count}</strong>
+
+            <button onClick={() => {
+                setUserInfo({
+                    ...userInfo,
+                    username: 'marvin',
+                    password: '123456'
+                })
+            }}>click
+            </button>
+            <button onClick={() => {
+                setCount(count + 1)
+            }}>count
+            </button>
         </>
     );
 };
